ab47a2ccedea226234af5aba313f9f779411f03e
ur mom <maxrofra@gmail.com>
2020-11-18 00:06:43 +0100

    Changes so far

---
+++ XY_beta/SplashScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreen.swift
+//  XY_beta
+//
+//  Created by Maxime Franchot on 17/11/2020.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
---
+++ XY_beta/ViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController.swift
+//  XY_beta
+//
+//  Created by Maxime Franchot on 17/11/2020.
+//
+
+import Foundation
