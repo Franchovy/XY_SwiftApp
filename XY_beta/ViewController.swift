035fb075b13964ba3e67307a0d78a8fd58c0579d
ur mom <maxrofra@gmail.com>
2020-11-18 17:35:47 +0100

    Cleaned ViewController file

--- XY_beta/ViewController.swift
+++ XY_beta/ViewController.swift
@@ -16,38 +16,12 @@ struct SwiftUIViewController: UIViewControllerRepresentable {
         
         let storyboard = UIStoryboard(name: "Main",bundle: Bundle.main)
         let controller = storyboard.instantiateViewController(identifier: "Home")
+    
         
         return controller
     }
     
     func updateUIViewController(_ uiViewController: SwiftUIViewController.UIViewControllerType, context: UIViewControllerRepresentableContext<SwiftUIViewController>) {
-        
-    }
-    
-}
-
-class ViewController: UIViewController {
-
-    let label:UILabel = {
-        let label = UILabel()
-        label.text = "Hello World"
-        label.font = UIFont.boldSystemFont(ofSize: 40)
-        label.textColor = .systemBlue
-        label.contentMode = .center
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.view.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-        ])
         
     }
     
