b304aa57ac96e723f0952c511c138c5cca75757b
bucciolsimone@gmail.com <bucciolsimone@gmail.com>
2020-11-18 17:20:53 +0100

    yeeee

--- XY_beta/ContentView.swift
+++ XY_beta/ContentView.swift
@@ -13,51 +13,16 @@ let DISPLAY_SWIFT_UI = false   //
 
 struct ContentView: View {
     
-    init() {
-        UITabBar.appearance().backgroundColor = UIColor.black
-        UITabBar.appearance().isTranslucent = true
-       
-    }
+
 
     var body: some View {
         // SwiftUI View
-        if (DISPLAY_SWIFT_UI) {
-            TabView {
-                Text("Home")
-                    .tabItem {
-                        Image(systemName:"house.fill")
-                        Text("Home")
-                    }
-                Text("Communities")
-                    .tabItem {
-                        Image(systemName:"person.3.fill")
-                        Text("Communities")
-                    }
-                Text("Notifications")
-                    .tabItem {
-                        Image(systemName:"bell.fill")
-                        Text("Notifications")
-                        
-                    }
-                Text("XYshop")
-                    .tabItem {
-                        Image(systemName:"bag.fill")
-                        Text("XYshop")
-                    }
-                Text("Profile")
-                    .tabItem {
-                        Image(systemName:"square.fill")
-                        Text("Profile")
-                        
-                    }
-            }
-            .accentColor(.blue)
-        } else {
+       
             // StoryBoard / UIKit View
             SwiftUIView()
         }
     }
-}
+
         
 
 struct ContentView_Previews: PreviewProvider {
