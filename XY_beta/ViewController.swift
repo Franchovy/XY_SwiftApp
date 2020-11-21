32d67db1e40b42be748bb206d95741ed0984ab98
ur mom <maxrofra@gmail.com>
2020-11-21 13:11:02 +0100

    Changed project for a native Storyboard project

--- XY_beta/ViewController.swift
+++ XY_beta/ViewController.swift
@@ -2,26 +2,18 @@
 //  ViewController.swift
 //  XY_beta
 //
-//  Created by Maxime Franchot on 17/11/2020.
+//  Created by Maxime Franchot on 21/11/2020.
 //
 
-import Foundation
-import SwiftUI
 import UIKit
 
-struct ViewController: UIViewControllerRepresentable {
-    typealias UIViewControllerType = UIViewController
-    
-    func makeUIViewController(context: UIViewControllerRepresentableContext<ViewController>) -> ViewController.UIViewControllerType {
-        
-        let storyboard = UIStoryboard(name: "Main",bundle: Bundle.main)
-        let controller = storyboard.instantiateViewController(identifier: "Home")
-        
-        return controller
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
-    
-    func updateUIViewController(_ uiViewController: ViewController.UIViewControllerType, context: UIViewControllerRepresentableContext<ViewController>) {
-        
-    }
-    
+
+
 }
+
