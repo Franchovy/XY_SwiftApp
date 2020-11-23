141f9dca9fd0cbd35f0f1acc6a484a7d913bcfc8
ur mom <maxrofra@gmail.com>
2020-11-23 01:03:42 +0100

    Added HTTP debugger screen and call backend functionality

--- XY_APP/ViewController.swift
+++ XY_APP/ViewController.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-let API_URL = "192.168.43.155"
+let API_URL = "http://192.168.43.155:5000"
 
 class ViewController: UIViewController {
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-     
+        
     }
     
 
@@ -35,34 +35,117 @@ class ViewController: UIViewController {
     
     //MARK: - Interaction
     
-    @IBAction func loginRequestButton(_ sender: Any) {
-        guard let url = URL(string: API_URL + "/get") else { return }
+    
+
+    @IBAction func LoginButtonPressed(_ sender: Any) {
+        // Send Signup to url
+        guard let url = URL(string: API_URL + "/login") else { return }
         
         var loginRequest = URLRequest(url: url)
-        loginRequest.httpMethod = "POST"
+        loginRequest.httpMethod = "GET"
         
         do {
-            let params = ["emailaddress": "maxrofra@gmail.com", "password": "secretWord"]
-            loginRequest.httpBody = try JSONSerialization.data(withJSONObject: params, options: .init())
-            
-            URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            URLSession.shared.dataTask(with: loginRequest) { (data, resp, err) in
                 if let err = err {
                     print("Failed to login:", err)
                     return
                 }
                 
-                print("Probably logged in successfully")
+                print("Data: ", data)
+                
+                
+                loginRequest.httpMethod = "POST"
+                do {
+                    let params = "username=maxime&password=secretword&submit=1&remember_me=1".data(using: .utf8)
+                    loginRequest.httpBody = params
+                    
+                    
+                    URLSession.shared.dataTask(with: loginRequest) { (data, resp, err) in
+                        if let err = err {
+                            print("Failed to login:", err)
+                            return
+                        }
+                        print(resp)
+                        // Check login session authenticated.
+                        if let data = data,
+                                let urlContent = NSString(data: data, encoding: String.Encoding.ascii.rawValue) {
+                                print(urlContent)
+                            } else {
+                                print("Error: \(err)")
+                            }
+                    }.resume()
+                } catch {
+                    print("Failed to serialize data:", error)
+                }
+            }.resume()
+        } catch {
+            print("Error bruh")
+        }
+    }
+}
+
+
+class DebuggerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+     
+    }
+    
+    @IBAction func CheckLoginButtonPressed(_ sender: Any) {
+        // Check if login session is open.
+        
+        // Send Request to url
+        guard let url = URL(string: API_URL + "/auth") else { return }
+        
+        var loginRequest = URLRequest(url: url)
+        loginRequest.httpMethod = "GET"
+        
+        do {
+            URLSession.shared.dataTask(with: url) { (data, resp, err) in
+                if let err = err {
+                    print("Failed to get login session:", err)
+                    return
+                }
+                
+                if let data = data,
+                        let urlContent = NSString(data: data, encoding: String.Encoding.ascii.rawValue) {
+                        print(urlContent)
+                        print("Probably has login session.")
+                    } else {
+                        print("Error: \(err)")
+                    }
+            }.resume()
+        } catch {
+            print("Failed to serialize data:", error)
+        }
+    }
+    @IBAction func CheckCSRFButtonPressed(_ sender: Any) {
+        // Get CSRF Token
+        guard let url = URL(string: API_URL + "/csrf") else { return }
+        
+        var csrfRequest = URLRequest(url: url)
+        csrfRequest.httpMethod = "GET"
+        
+        do {
+            URLSession.shared.dataTask(with: csrfRequest) { (data, resp, err) in
+                if let err = err {
+                    print("Failed to login:", err)
+                    return
+                }
+                print(resp)
+                // Check login session authenticated.
+                if let data = data,
+                        let urlContent = NSString(data: data, encoding: String.Encoding.ascii.rawValue) {
+                        print(urlContent)
+                    } else {
+                        print("Error: \(err)")
+                    }
             }.resume()
         } catch {
             print("Failed to serialize data:", error)
         }
     }
     
-    @IBAction func postRequestButton(_ sender: UIButton) {
-        let url = URL(string:  API_URL + "/post")!
-        _ = URLRequest(url: url)
-        
-        
-    }
 }
-
