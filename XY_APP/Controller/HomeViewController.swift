56e9904b0b34925346be62565cd0f5f94c2ee364
ur mom <maxrofra@gmail.com>
2020-11-28 12:13:29 +0100

    Added home page tableview

--- XY_APP/Controller/HomeViewController.swift
+++ XY_APP/Controller/HomeViewController.swift
@@ -7,14 +7,77 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-    @IBOutlet weak var tabBar: UITabBar!
 
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Data model: These strings will be the data for the table view cells
+    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    // These are the colors of the square views in our table view cells.
+    // In a real project you might use UIImages.
+    let colors = [UIColor.blue, UIColor.yellow, UIColor.magenta, UIColor.red, UIColor.brown]
+    
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    // don't forget to hook this up from the storyboard
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Dynamic view load: Success")
         
+        // Register the table view cell class and its reuse id
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        // Remove the extra empty cell divider lines
+        self.tableView.tableFooterView = UIView()
+
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.animals.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:MyCustomCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! MyCustomCell
+        
+        // set the text from the data model
+        cell.myView.backgroundColor = self.colors[indexPath.row]
+                cell.myCellLabel.text = self.animals[indexPath.row]
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+}
+
+
+class MyCustomCell: UITableViewCell {
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myCellLabel: UILabel!
+}
+
+class ViewTableCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
     }
 }
 
---
+++ XY_APP/Model/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  XY_APP
+//
+//  Created by Maxime Franchot on 28/11/2020.
+//
+
+import Foundation
+import UIKit
+
+struct Post {
+    var image = UIImage(named: "technology rectangle")
+    var username = "Test user"
+}
