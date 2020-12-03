1ee1a4c444d1559d20983ce8e341882560b19b5b
bucciolsimone@gmail.com <bucciolsimone@gmail.com>
2020-12-03 19:50:05 +0100

    Enabled profile camera

--- XY_APP/Controller/ProfileViewController.swift
+++ XY_APP/Controller/ProfileViewController.swift
@@ -2,59 +2,50 @@
 //  ProfileViewController.swift
 //  XY_APP
 //
-//  Created by Maxime Franchot on 03/12/2020.
+//  Created by Simone on 03/12/2020.
 //
 
 import UIKit
 
-class ProfileViewController : UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBOutlet weak var coverPicture: UIImageView!
-    @IBOutlet weak var profileCoverImage: UIImageView!
-    @IBOutlet weak var profileDash: UIView!
-    @IBOutlet weak var followButton: UIButton!
+    let imagePicker = UIImagePickerController()
     
+    @IBOutlet weak var buttonsConsole: UIView!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var profileConteiner: UIView!
+   
     override func viewDidLoad() {
+        
+        profileConteiner.layer.cornerRadius = 15.0
+        profileConteiner.layer.shadowColor = UIColor.black.cgColor
+        profileConteiner.layer.shadowOffset = CGSize(width:1, height:1)
+        profileConteiner.layer.shadowRadius = 2
+        profileConteiner.layer.shadowOpacity = 1.0
+        
+        buttonsConsole.layer.cornerRadius = 15.0
+        buttonsConsole.layer.shadowColor = UIColor.black.cgColor
+        buttonsConsole.layer.shadowOffset = CGSize(width:1, height:1)
+        buttonsConsole.layer.shadowRadius = 2
+        buttonsConsole.layer.shadowOpacity = 1.0
+        
         super.viewDidLoad()
         
-        profileCoverImage.layer.cornerRadius = 15.0
-        profileCoverImage.layer.shadowColor = UIColor.black.cgColor
-        profileCoverImage.layer.shadowOffset = CGSize(width:1, height:1)
-        profileCoverImage.layer.shadowRadius = 2
-        profileCoverImage.layer.shadowOpacity = 1.0
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
         
-        profileDash.layer.cornerRadius = 15.0
-        profileDash.layer.shadowColor = UIColor.black.cgColor
-        profileDash.layer.shadowOffset = CGSize(width:1, height:1)
-        profileDash.layer.shadowRadius = 2
-        profileDash.layer.shadowOpacity = 1.0
+        let logo = UIImage(named: "XYnavbarlogo")
+        let imageView = UIImageView(image:logo)
+        self.navigationItem.titleView = imageView
         
-        followButton.layer.cornerRadius = 5.0
-        followButton.layer.shadowColor = UIColor.black.cgColor
-        followButton.layer.shadowOffset = CGSize(width:1, height:1)
-        followButton.layer.shadowRadius = 2
-        followButton.layer.shadowOpacity = 1.0
+        
         
         
     }
-    
-    
-    @IBAction func uploadImageButtonPressed(_ sender: Any) {
-        // Run a Async thread to upload file in order not to slow down the user experience
-        //DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            if let image = coverPicture.image {
-                Profile(username:"test").uploadImageOne(image: image, completion: { result in
-                    switch result {
-                    case .success(let message):
-                        print("Image Upload Success!")
-                    case .failure(let error):
-                        print("Image Upload Failure.")
-                    }
-                })
-            }
-        //}
+
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+        
+        
     }
-    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
-    }
-    
 }
