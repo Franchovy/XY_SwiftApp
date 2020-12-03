654662ca9b3a977225f66c3abb9773af85438350
bucciolsimone@gmail.com <bucciolsimone@gmail.com>
2020-12-03 19:17:43 +0100

    Added latest stash

---
+++ XY_APP/Controller/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  XY_APP
+//
+//  Created by Simone on 03/12/2020.
+//
+
+import UIKit
+
+class ProfileViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    override func viewDidLoad() {
+        
+        
+        
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+    }
+    
+    
+}
