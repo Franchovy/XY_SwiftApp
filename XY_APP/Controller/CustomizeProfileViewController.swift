b63f26617244eec8068698c11e3aa7ccadc17c33
bucciolsimone@gmail.com <bucciolsimone@gmail.com>
2020-12-05 17:19:16 +0100

    Allow photo from camera roll

--- XY_APP/Controller/CustomizeProfileViewController.swift
+++ XY_APP/Controller/CustomizeProfileViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 
-class CustomizeProfileViewController: UIViewController {
+class CustomizeProfileViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var containerOne: UIView!
     
@@ -28,4 +28,21 @@ class CustomizeProfileViewController: UIViewController {
        
     }
 
+    @IBAction func editProfileButton(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+            picker.delegate = self
+            let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: {
+                action in
+                picker.sourceType = .camera
+                self.present(picker, animated: true, completion: nil)
+            }))
+            alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {
+                action in
+                picker.sourceType = .photoLibrary
+                self.present(picker, animated: true, completion: nil)
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+    }
 }
--- XY_APP/Controller/ProfileViewController.swift
+++ XY_APP/Controller/ProfileViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Simone on 03/12/2020.
 //
 
+import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -13,12 +14,19 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     let imagePicker = UIImagePickerController()
     #endif
     
+    @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var buttonsConsole: UIView!
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var profileConteiner: UIView!
     @IBOutlet weak var coverPicture: UIImageView!
     
     override func viewDidLoad() {
+        
+        profileImage.layer.borderWidth = 1
+        profileImage.layer.masksToBounds = false
+        profileImage.layer.borderColor = UIColor.white.cgColor
+        profileImage.layer.cornerRadius = profileImage.frame.height/2
+        profileImage.clipsToBounds = true
         
         profileConteiner.layer.cornerRadius = 15.0
         profileConteiner.layer.shadowColor = UIColor.black.cgColor
@@ -39,21 +47,44 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         imagePicker.sourceType = .camera
         #endif
         
+        
+        // nav bar logo
         let logo = UIImage(named: "XYnavbarlogo")
         let imageView = UIImageView(image:logo)
         self.navigationItem.titleView = imageView
     }
     
-    @IBAction func submitPostButtonPressed(_ sender: Any) {
-
-    }
+  
     
-
+    @IBAction func editProfileImagePresed(_ sender: AnyObject) {
+        let picker = UIImagePickerController()
+            picker.delegate = self
+            let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: {
+                action in
+                picker.sourceType = .camera
+                self.present(picker, animated: true, completion: nil)
+            }))
+            alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {
+                action in
+                picker.sourceType = .photoLibrary
+                self.present(picker, animated: true, completion: nil)
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+   
     @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
-        //Camera should only be used not in the simulator
         #if !targetEnvironment(simulator)
         present(imagePicker, animated: true, completion: nil)
         #endif
-        
     }
-}
+    
+    }
+
+    
+
+
+
+
