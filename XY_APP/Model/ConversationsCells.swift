e7a7cb1b603d79cc60e63def11cd6387c99e0ff1
bucciolsimone@gmail.com <bucciolsimone@gmail.com>
2020-12-08 14:43:41 +0100

    storyboards and xib

---
+++ XY_APP/Model/ConversationsCells.swift
@@ -0,0 +1,23 @@
+//
+//  ConversationsCells.swift
+//  XY_APP
+//
+//  Created by Simone on 07/12/2020.
+//
+
+import UIKit
+
+class ConversationsCells: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
