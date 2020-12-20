fbe7b801088994c55e68fdfec43a9228b0986074
bucciolsimone@gmail.com <bucciolsimone@gmail.com>
2020-12-20 14:21:05 +0100

    a lot of new stuff

--- XY_APP/Controller/ConversationsViewController.swift
+++ XY_APP/Controller/ConversationsViewController.swift
@@ -9,16 +9,16 @@ import Foundation
 import UIKit
 
 class ConversationsViewController: UIViewController {
-
+    
     @IBOutlet weak var conversationsTableView: UITableView!
     var conversation : [Conversations] = [
-    
+        
         Conversations(profileImage:"maxime profile image", name: "Elon Musk" , message: "Hello maaaan", time: "1 min ago"),
         
         Conversations(profileImage: "maxime profile image", name: "Maxime Franchot" , message: "Bro let's go run ", time: "2 mins ago"),
-         
+        
         Conversations(profileImage: "maxime profile image", name: "Diana" , message: "Remmeber to take the umbrella", time: "3 mins ago")
-    
+        
     ]
     
     override func viewDidLoad() {
@@ -27,11 +27,11 @@ class ConversationsViewController: UIViewController {
         conversationsTableView.dataSource = self
         conversationsTableView.layer.cornerRadius = 15.0
         
-       
-
+        
         let logo = UIImage(named: "XYnavbarlogo")
         let imageView = UIImageView(image:logo)
         self.navigationItem.titleView = imageView
+        conversationsTableView.register(UINib(nibName: "ConversationChat", bundle: nil), forCellReuseIdentifier: "ReusableCell")
         
     }
 }
@@ -42,12 +42,9 @@ extension ConversationsViewController : UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
-        cell.textLabel?.text = "This is a conversation"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! ConversationChat
+        cell.messageReceived.text = "Porco Dio"
         return cell
     }
-    
-    
-    
 }
 
