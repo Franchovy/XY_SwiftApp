11f630f74f71b6cce0a379584cfd4b50f7d15554
bucciolsimone@gmail.com <bucciolsimone@gmail.com>
2021-01-03 11:06:36 +0100

    saved changes again

---
+++ XY_APP/Controller/TabBar/UpperProfileModel.swift
@@ -0,0 +1,21 @@
+//
+//  UpperProfileModel.swift
+//  XY_APP
+//
+//  Created by Simone on 02/01/2021.
+//
+
+import Foundation
+import UIKit
+
+struct UpperProfile {
+    
+    var Nickname: String
+    var ProfileImage: UIImage
+    var Link: String
+    var Followers: String
+    var Following: String
+    var Level: String
+    var ProfileCaption: String
+    
+}
