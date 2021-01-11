0aa6d6bd04b719ae7151beba4c4980edc431d57b
ur mom <maxrofra@gmail.com>
2021-01-11 14:52:04 +0100

    Fixed scrolling, removed cell selection, added basic swipe pan gesture recogniser

--- XY_APP/View/ImagePostCell.swift
+++ XY_APP/View/ImagePostCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseStorage
+import AVFoundation
 
 class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
     func profileImageDownloadProgress(progress: Float) {
@@ -16,7 +17,6 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
     func postImageDownloadProgress(progress: Float) {
         
     }
-    
     
     // MARK: - PROPERTIES
     
@@ -55,6 +55,10 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
     
     // MARK: - IBOutlets
     
+    
+    
+    @IBOutlet weak var postCard: UIView!
+    
     @IBOutlet weak var xpLevelDisplay: CircleView!
     
     @IBOutlet weak var nameLabelLeftConstraint: NSLayoutConstraint!
@@ -76,6 +80,10 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
     
     // MARK: - PUBLIC METHODS
     
+    var swipeRightGesture:UISwipeGestureRecognizer!
+    var swipeLeftGesture:UISwipeGestureRecognizer!
+    var panGesture:UIPanGestureRecognizer!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -86,6 +94,20 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
         timestampLabel.alpha = 1
         nameLabel.alpha = 1
         contentLabel.alpha = 1
+        
+        swipeRightGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight(swipeGestureRecognizer:)))
+        swipeRightGesture.direction = .right
+        //addGestureRecognizer(swipeRightGesture)
+        
+        swipeLeftGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft(swipeGestureRecognizer:)))
+        swipeLeftGesture.direction = .left
+        //addGestureRecognizer(swipeLeftGesture)
+        
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture(panGestureRecognizer:)))
+        panGesture.delegate = self
+        
+        panGesture.isEnabled = true
+        addGestureRecognizer(panGesture)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -101,5 +123,101 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
         
     }
     
+    var isSwipedRight = false
+    var isSwipedLeft = false
+    
+    //
+    
+    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let gesture = gestureRecognizer as? UIPanGestureRecognizer else {fatalError()}
+        
+        let velocity = gesture.velocity(in: nil)
+        return abs(velocity.x) > abs(velocity.y)
+    }
+    
+    @objc func panGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+        let translation = panGestureRecognizer.translation(in: nil)
+        
+        postCard.transform.tx = translation.x
+    }
+    
+    @objc func swipeLeft(swipeGestureRecognizer: UISwipeGestureRecognizer) {
+        
+        if !isSwipedLeft || isSwipedRight {
+            isSwipedLeft = !isSwipedRight
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
+                if self.isSwipedRight {
+                    self.transform.tx = 0
+                } else {
+                    self.transform = CGAffineTransform(translationX: -500, y: 0)
+                }
+            }, completion: { bool in
+                if self.isSwipedRight {
+                    self.isSwipedRight = false
+                } else {
+                    // Swipe left execution
+                    print("swipe left!")
+                }
+            })
+        }
+    }
+    
+    @objc func swipeRight(swipeGestureRecognizer: UISwipeGestureRecognizer) {
+        if !isSwipedRight || isSwipedLeft {
+            isSwipedRight = !isSwipedLeft
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
+                if self.isSwipedLeft {
+                    self.transform.tx = 0
+                } else {
+                    self.transform = CGAffineTransform(translationX: 150, y: 0)
+                    
+                    self.alpha = 0.5
+                }
+            }, completion: { bool in
+                if self.isSwipedLeft {
+                    self.isSwipedLeft = false
+                } else {
+                    // Swipe right execution
+                    print("Swipe right!")
+                }
+            })
+        } else if isSwipedRight {
+            // Swipe right confirmation
+            print("Confirm swipe right!")
+            confirmSwipeRight()
+        }
+    }
+    
+    func confirmSwipeRight() {
+        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 20, options: .curveEaseIn, animations: {
+            self.transform.tx = 300
+        }, completion: { bool in
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
+                self.transform.tx = 0
+            })
+        })
+    }
+    
+    enum SwipeDirection {
+        case left
+        case right
+    }
+    
+    func removePostFromFlow(direction: SwipeDirection) {
+        guard let flowVC = viewContainingController() as? FlowVC else {fatalError()}
+            
+        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
+            self.transform.tx = 500
+        }, completion: { bool in
+            flowVC.barXPCircle.progressBarCircle.color = .blue
+            
+            // Collapse this view
+            let indexPath = flowVC.tableView.indexPath(for: self)!
+            //flowVC.removeCell()
+            flowVC.data.remove(at: indexPath.row)
+            flowVC.tableView.deleteRows(at: [indexPath], with: .right)
+        })
+    }
 }
 
