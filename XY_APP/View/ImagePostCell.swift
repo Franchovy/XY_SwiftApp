a432a156d84a399e73945260078a31da5ba823aa
ur mom <maxrofra@gmail.com>
2021-01-11 19:53:24 +0100

    Added swipe right and swipe left movement and animations

--- XY_APP/View/ImagePostCell.swift
+++ XY_APP/View/ImagePostCell.swift
@@ -55,8 +55,6 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
     
     // MARK: - IBOutlets
     
-    
-    
     @IBOutlet weak var postCard: UIView!
     
     @IBOutlet weak var xpLevelDisplay: CircleView!
@@ -80,8 +78,6 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
     
     // MARK: - PUBLIC METHODS
     
-    var swipeRightGesture:UISwipeGestureRecognizer!
-    var swipeLeftGesture:UISwipeGestureRecognizer!
     var panGesture:UIPanGestureRecognizer!
     
     override func awakeFromNib() {
@@ -94,14 +90,6 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
         timestampLabel.alpha = 1
         nameLabel.alpha = 1
         contentLabel.alpha = 1
-        
-        swipeRightGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight(swipeGestureRecognizer:)))
-        swipeRightGesture.direction = .right
-        //addGestureRecognizer(swipeRightGesture)
-        
-        swipeLeftGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft(swipeGestureRecognizer:)))
-        swipeLeftGesture.direction = .left
-        //addGestureRecognizer(swipeLeftGesture)
         
         panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture(panGestureRecognizer:)))
         panGesture.delegate = self
@@ -123,80 +111,111 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
         
     }
     
-    var isSwipedRight = false
-    var isSwipedLeft = false
-    
     //
     
     override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        guard let gesture = gestureRecognizer as? UIPanGestureRecognizer else {fatalError()}
+        if let gesture = gestureRecognizer as? UIPanGestureRecognizer {
+            let velocity = gesture.velocity(in: nil)
+            return abs(velocity.x) > abs(velocity.y)
+        }
+        else { return true }
         
-        let velocity = gesture.velocity(in: nil)
-        return abs(velocity.x) > abs(velocity.y)
     }
+    
+    var isSwipedRightXPView = false
+    static let defaultPanSensitivity = 2.3
+    var panSensitivity = defaultPanSensitivity
     
     @objc func panGesture(panGestureRecognizer: UIPanGestureRecognizer) {
         let translation = panGestureRecognizer.translation(in: nil)
         
-        postCard.transform.tx = translation.x
-    }
-    
-    @objc func swipeLeft(swipeGestureRecognizer: UISwipeGestureRecognizer) {
+        postCard.transform.tx = postCard.transform.tx + translation.x * CGFloat(panSensitivity)
         
-        if !isSwipedLeft || isSwipedRight {
-            isSwipedLeft = !isSwipedRight
+        if panGestureRecognizer.state == .ended {
+            panSensitivity = ImagePostCell.defaultPanSensitivity
             
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
-                if self.isSwipedRight {
-                    self.transform.tx = 0
-                } else {
-                    self.transform = CGAffineTransform(translationX: -500, y: 0)
+            // On pan release
+            if translation.x > 0 {
+                // Direction: Right
+                if isSwipedRightXPView {
+                    // Confirm Swipe Right
+                    isSwipedRightXPView = false
+                    print("Confirm swipe right")
+                    confirmSwipe(direction: .right)
                 }
-            }, completion: { bool in
-                if self.isSwipedRight {
-                    self.isSwipedRight = false
+                
+                if panGestureRecognizer.velocity(in: nil).x > 150
+                    && translation.x > 50 {
+                    // Confirm swipe right
+                    confirmSwipe(direction: .right)
+                    print("Direct swipe right")
+                } else if translation.x < 30 {
+                    // Cancel gesture
+                    cancelSwipe()
+                    print("Cancel swipe")
                 } else {
-                    // Swipe left execution
-                    print("swipe left!")
+                    // XP Circle
+                    swipeRightXPView()
+                    print("Swipe Right XP View")
                 }
-            })
-        }
-    }
-    
-    @objc func swipeRight(swipeGestureRecognizer: UISwipeGestureRecognizer) {
-        if !isSwipedRight || isSwipedLeft {
-            isSwipedRight = !isSwipedLeft
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
-                if self.isSwipedLeft {
-                    self.transform.tx = 0
+            } else {
+                // Direction: Left
+                if isSwipedRightXPView {
+                    isSwipedRightXPView = false
+                    panSensitivity = 1.0
+                    cancelSwipe()
                 } else {
-                    self.transform = CGAffineTransform(translationX: 150, y: 0)
+                    // Swipe Left
+                    if translation.x > -100 {
+                        // Cancel gesture
+                        cancelSwipe()
+                    } else {
+                        // Swipe Left
+                        confirmSwipe(direction: .left)
+                    }
+                }
+            }
+        } else {
+            // Transform while holding
+            if isSwipedRightXPView && translation.x < 0 {
+                isSwipedRightXPView = false
+                cancelSwipe()
+                return
+            } else {
+                if #available(iOS 13.0, *) {
                     
-                    self.alpha = 0.5
+                    let PI = CGFloat(3.1415)
+                    let sensitivity = CGFloat(0.001)
+                    let angle = (translation.x * sensitivity * PI / 2) - PI / 2
+                    
+                    postCard.transform3D.m11 = -sin(angle)
+                    postCard.transform3D.m12 = cos(angle)
+                    postCard.transform3D.m31 = sin(angle)
+                    postCard.transform3D.m32 = cos(angle)
+                    postCard.transform3D.m23 = 1
+                    postCard.transform3D.m44 = 1
                 }
-            }, completion: { bool in
-                if self.isSwipedLeft {
-                    self.isSwipedLeft = false
-                } else {
-                    // Swipe right execution
-                    print("Swipe right!")
-                }
-            })
-        } else if isSwipedRight {
-            // Swipe right confirmation
-            print("Confirm swipe right!")
-            confirmSwipeRight()
+            }
         }
     }
     
-    func confirmSwipeRight() {
-        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 20, options: .curveEaseIn, animations: {
-            self.transform.tx = 300
+    func cancelSwipe() {
+        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseIn, animations: {
+            self.postCard.transform.tx = 0
+            
         }, completion: { bool in
-            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
-                self.transform.tx = 0
-            })
+            
         })
+    }
+    
+    func swipeRightXPView() {
+        UIView.animate(withDuration: 0.8, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 5, options: .curveEaseIn, animations: {
+            self.postCard.transform.tx = 150
+            
+        }, completion: { bool in
+            self.isSwipedRightXPView = true
+        })
+        
     }
     
     enum SwipeDirection {
@@ -204,19 +223,34 @@ class ImagePostCell: UITableViewCell, FlowDataCell, PostViewModelDelegate {
         case right
     }
     
-    func removePostFromFlow(direction: SwipeDirection) {
-        guard let flowVC = viewContainingController() as? FlowVC else {fatalError()}
+    func confirmSwipe(direction: SwipeDirection) {
+        let directionMultiplier = direction == .left ? -1 : 1
+        
+        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 5.0, initialSpringVelocity: 20, options: .curveEaseIn, animations: {
+            self.postCard.transform.tx = 500 * CGFloat(directionMultiplier)
             
-        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
-            self.transform.tx = 500
         }, completion: { bool in
-            flowVC.barXPCircle.progressBarCircle.color = .blue
-            
-            // Collapse this view
-            let indexPath = flowVC.tableView.indexPath(for: self)!
-            //flowVC.removeCell()
-            flowVC.data.remove(at: indexPath.row)
-            flowVC.tableView.deleteRows(at: [indexPath], with: .right)
+            if direction == .left {
+                UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 10, options: .curveEaseOut, animations: {
+                    self.postCard.transform.tx = 500 * CGFloat(directionMultiplier)
+                }, completion: { bool in
+                    guard let flowVC = self.viewContainingController() as? FlowVC else {fatalError()}
+                    
+                    flowVC.barXPCircle.progressBarCircle.color = .blue
+                    
+                    // Collapse this view
+                    let indexPath = flowVC.tableView.indexPath(for: self)!
+                    
+                    flowVC.data.remove(at: indexPath.row)
+                    flowVC.tableView.deleteRows(at: [indexPath], with: direction == .right ? .right : .left)
+                })
+            } else {
+                UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 1.0, initialSpringVelocity: 5, options: .curveEaseOut, animations: {
+                    self.postCard.transform.tx = 0
+                }, completion: { bool in
+                    
+                })
+            }
         })
     }
 }
