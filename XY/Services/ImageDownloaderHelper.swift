55e183f7c9921b97089cea99e87d8c86b828cdc2
ur mom <maxrofra@gmail.com>
2021-01-27 21:39:08 +0100

    Removed caching code from ImageHelper

--- XY/Services/ImageDownloaderHelper.swift
+++ XY/Services/ImageDownloaderHelper.swift
@@ -11,29 +11,18 @@ import FirebaseStorage
 class ImageDownloaderHelper {
     static var shared = ImageDownloaderHelper()
     private init() { }
-    
-    var cachedImageURLs = [String: URL]()
-    
+        
     func getFullURL(imageId: String, completion: @escaping(URL?, Error?) -> Void) {
-        if let cachedImageURL = cachedImageURLs[imageId] {
-            print("Returning cached image")
-            completion(cachedImageURL, nil)
-        } else {
-            let storage = Storage.storage()
-            let ref = storage.reference(withPath: imageId)
-            ref.downloadURL() { url, error in
-                guard error == nil else {
-                    completion(nil, error)
-                    return
-                }
-                
-                if let url = url {
-                    self.cachedImageURLs[imageId] = url
-                }
-                
-                completion(url, nil)
-                
+        let storage = Storage.storage()
+        let ref = storage.reference(withPath: imageId)
+        print("Fetching image: \(imageId)")
+        ref.downloadURL() { url, error in
+            guard error == nil else {
+                completion(nil, error)
+                return
             }
+            
+            completion(url, nil)
         }
     }
 }
