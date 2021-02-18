b34900e5d0b06debb846836e185bb4803b5bf34a
ur mom <maxrofra@gmail.com>
2021-02-18 12:36:00 +0100

    created UserModel and ProfileLivePostsViewController

---
+++ XY/Controller/Profile Header/ProfileLivePostsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileLivePostsViewController.swift
+//  XY
+//
+//  Created by Maxime Franchot on 18/02/2021.
+//
+
+import UIKit
+
+class ProfileLivePostsViewController: UIViewController {
+
+    
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        view.backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+}
