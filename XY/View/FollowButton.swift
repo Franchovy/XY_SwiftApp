9cbf96996a16d9cd235a1b4fcb29541fd62d6643
ur mom <maxrofra@gmail.com>
2021-03-20 17:43:32 +0100

    Made friends button look AMAZING

--- XY/View/FollowButton.swift
+++ XY/View/FollowButton.swift
@@ -11,6 +11,8 @@ import FaveButton
 class FollowButton: FaveButton {
     var relationshipType: RelationshipTypeForSelf?
     var otherUserID: String?
+    
+    var borderGradientLayer: CAGradientLayer?
 
     init() {
         super.init(frame: .zero)
@@ -22,14 +24,25 @@ class FollowButton: FaveButton {
         setTitleColor(.lightGray, for: .disabled)
         setTitle("Follow", for: .normal)
         titleLabel?.font = UIFont(name: "Raleway-ExtraBold", size: 16)
-        layer.borderWidth = 0.7
-        layer.borderColor = UIColor.white.cgColor
         
         addTarget(self, action: #selector(onPress), for: .touchUpInside)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        borderGradientLayer?.frame = bounds
+        
+        let shape = CAShapeLayer()
+        shape.lineWidth = 2
+        shape.path = UIBezierPath(roundedRect: bounds.insetBy(dx: 2, dy: 2), cornerRadius: height/2).cgPath
+        shape.strokeColor = UIColor.black.cgColor
+        shape.fillColor = UIColor.clear.cgColor
+//        borderGradientLayer?.mask = shape
     }
     
     public func configure(for relationshipType: RelationshipTypeForSelf, otherUserID: String) {
@@ -48,23 +61,29 @@ class FollowButton: FaveButton {
         case .following:
             setTitle("Subscribed", for: .normal)
             titleLabel?.font = UIFont(name: "Raleway-ExtraBold", size: 12)
-            setBackgroundColor(color: .gray, forState: .normal)
+            setBackgroundColor(color: .clear, forState: .normal)
+            layer.borderColor = UIColor.white.cgColor
+            layer.borderWidth = 1
             setAnimationsEnabled(enabled: false)
         case .follower:
             setTitle("Subscriber", for: .normal)
             titleLabel?.font = UIFont(name: "Raleway-ExtraBold", size: 12)
             setBackgroundColor(color: .gray, forState: .normal)
             setAnimationsEnabled(enabled: true)
+            resetBorder()
         case .friends:
             setTitle("Friends", for: .normal)
             titleLabel?.font = UIFont(name: "Raleway-ExtraBold", size: 14)
-            setBackgroundColor(color: UIColor(named: "XYpink")!, forState: .normal)
+            setBackgroundColor(color: .clear, forState: .normal)
+            resetBorder()
+            setBorderGradient()
             setAnimationsEnabled(enabled: false)
         case .none:
             setTitle("Subscribe", for: .normal)
             titleLabel?.font = UIFont(name: "Raleway-ExtraBold", size: 12)
             setBackgroundColor(color: UIColor(0x007BF5), forState: .normal)
             setAnimationsEnabled(enabled: true)
+            resetBorder()
         }
     }
     
@@ -102,5 +121,31 @@ class FollowButton: FaveButton {
                 }
             }
         }
+    }
+    
+    private func resetBorder() {
+        borderGradientLayer?.removeFromSuperlayer()
+        
+        layer.borderWidth = 0
+    }
+    
+    private func setBorderGradient() {
+        let borderGradientLayer = CAGradientLayer()
+        borderGradientLayer.colors = Global.xyGradient.map({$0.cgColor})
+        borderGradientLayer.startPoint = CGPoint(x: 0, y: 0.7)
+        borderGradientLayer.endPoint = CGPoint(x: 1.0, y: 0.3)
+        borderGradientLayer.locations = [0.3, 0.7]
+        
+        borderGradientLayer.frame = bounds
+        let shape = CAShapeLayer()
+        shape.lineWidth = 2
+        shape.path = UIBezierPath(roundedRect: bounds.insetBy(dx: 2, dy: 2), cornerRadius: height/2).cgPath
+        shape.strokeColor = UIColor.black.cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        borderGradientLayer.mask = shape
+        
+        layer.addSublayer(borderGradientLayer)
+        
+        self.borderGradientLayer = borderGradientLayer
     }
 }
