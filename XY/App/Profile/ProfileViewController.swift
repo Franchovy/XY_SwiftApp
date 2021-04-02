227d6199205e0aa9596a1f0bd78a6aeff23ff9a5
Maxime <maxrofra@gmail.com>
2021-04-02 09:21:51 +0200

    Added challenge button

--- XY/App/Profile/ProfileViewController.swift
+++ XY/App/Profile/ProfileViewController.swift
@@ -16,6 +16,10 @@ class ProfileViewController: UIViewController {
     private let numFriendsLabel = Label(style: .body, fontSize: 25)
     private let numChallengesLabel = Label(style: .body, fontSize: 25)
     
+    private let startChallengeLabel = Label("Start a challenge with ", style: .body, fontSize: 15)
+    private let startChallengeNameLabel = Label(style: .nickname, fontSize: 15)
+    private let challengeButton = GradientBorderButtonWithShadow()
+    
     private var viewModel: ProfileViewModel?
     
     init() {
@@ -37,12 +41,19 @@ class ProfileViewController: UIViewController {
         view.addSubview(challengesLabel)
         view.addSubview(numFriendsLabel)
         view.addSubview(numChallengesLabel)
+        view.addSubview(startChallengeLabel)
+        view.addSubview(startChallengeNameLabel)
+        view.addSubview(challengeButton)
         
         friendButton.titleLabel?.font = UIFont(name: "Raleway-Heavy", size: 20)
         
         profileBubble.imageView.layer.borderWidth = 1
         profileBubble.imageView.layer.borderColor = UIColor(named: "XYTint")!.cgColor
-
+        
+        challengeButton.setBackgroundColor(color: UIColor(named: "XYBackground")!)
+        challengeButton.setGradient(Global.xyGradient)
+        challengeButton.setTitle("Challenge", for: .normal)
+        challengeButton.titleLabel?.font = UIFont(name: "Raleway-Heavy", size: 26)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -112,12 +123,39 @@ class ProfileViewController: UIViewController {
             height: challengesLabel.height
         )
         
+        startChallengeLabel.sizeToFit()
+        startChallengeNameLabel.sizeToFit()
+        
+        let startChallengeLabelsTotalWidth = startChallengeLabel.width + startChallengeNameLabel.width
+        
+        startChallengeLabel.frame = CGRect(
+            x: (view.width - startChallengeLabelsTotalWidth)/2,
+            y: view.height/2 - 80,
+            width: startChallengeLabel.width,
+            height: startChallengeLabel.height
+        )
+        
+        startChallengeNameLabel.frame = CGRect(
+            x: startChallengeLabel.right,
+            y: startChallengeLabel.top,
+            width: startChallengeNameLabel.width,
+            height: startChallengeNameLabel.height
+        )
+        
+        let challengeButtonSize = CGSize(width: 237, height: 50)
+        challengeButton.frame = CGRect(
+            x: (view.width - challengeButtonSize.width)/2,
+            y: startChallengeLabel.bottom + 17,
+            width: challengeButtonSize.width,
+            height: challengeButtonSize.height
+        )
     }
     
     public func configure(with viewModel: ProfileViewModel) {
         self.viewModel = viewModel
         
         navigationItem.title = viewModel.nickname
+        startChallengeNameLabel.text = viewModel.nickname
         
         numFriendsLabel.text = String(describing: viewModel.numFriends)
         numChallengesLabel.text = String(describing: viewModel.numChallenges)
