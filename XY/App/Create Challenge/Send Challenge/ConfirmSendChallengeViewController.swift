d450c2849986264c3f2e11ebd5a886f5e1009dce
Maxime <maxrofra@gmail.com>
2021-04-15 15:51:10 +0200

    Fixed turning hero on navigation controller back off

--- XY/App/Create Challenge/Send Challenge/ConfirmSendChallengeViewController.swift
+++ XY/App/Create Challenge/Send Challenge/ConfirmSendChallengeViewController.swift
@@ -45,6 +45,12 @@ class ConfirmSendChallengeViewController: UIViewController {
         navigationController?.isNavigationBarHidden = true
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.navigationController?.isHeroEnabled = false
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         HapticsManager.shared.vibrateImpact(for: .soft)
