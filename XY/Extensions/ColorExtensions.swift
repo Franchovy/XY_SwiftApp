6a2037b5e68e27a5b1c2f38154e90a9f76439665
Maxime <maxrofra@gmail.com>
2021-04-19 14:08:49 +0200

    Added UIColor XYColors extension

---
+++ XY/Extensions/ColorExtensions.swift
@@ -0,0 +1,41 @@
+//
+//  ColorExtensions.swift
+//  XY
+//
+//  Created by Maxime Franchot on 19/04/2021.
+//
+
+import UIKit
+
+
+extension UIColor {
+    static var XYWhite: UIColor {
+        get {
+            UIColor(named: "XYWhite")!
+        }
+    }
+    
+    static var XYBlack: UIColor {
+        get {
+            UIColor(named: "XYBlack")!
+        }
+    }
+    
+    static var XYBackground: UIColor {
+        get {
+            UIColor(named: "XYBackground")!
+        }
+    }
+    
+    static var XYCard: UIColor {
+        get {
+            UIColor(named: "XYCard")!
+        }
+    }
+    
+    static var XYTint: UIColor {
+        get {
+            UIColor(named: "XYTint")!
+        }
+    }
+}
